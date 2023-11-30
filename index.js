c6d00c2707f1d619b714be5adc3bd0502a0f725e
iHateeCpp <118418023+iHateeCpp@users.noreply.github.com>
2023-11-30 08:48:21 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+function isElementInViewport (el) {
+    var rect = el.getBoundingClientRect();
+
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /* or $(window).height() */
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth) /* or $(window).width() */
+    );
+}
+
+function scrollView(elemID) {
+    document.getElementById(elemID).scrollIntoView({behavior: "smooth", block: "center"});
+}
